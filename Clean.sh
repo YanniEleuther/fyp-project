b937a9c4cce8d49bf1bc9ed960dd7b99c56a6be7
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-04-14 14:29:06 -0400

    Extended Cleanup Script

--- Clean.sh
+++ Clean.sh
@@ -2,7 +2,14 @@
 
 alias remove='rm --recursive --force'
 
+clear
 
 remove nonrarevariants_exphunter
 remove c9orf72_exphunter
 remove fmr1_exphunter
+remove fmr1output.tsv
+remove json
+remove hgdp_references.tsv
+remove nonrare_catalog.tsv
+remove c9orf72output.tsv
+remove nonrares_output.tsv
